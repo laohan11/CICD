eb10f49b2245074d089ba0ab893f1256da21cca5
laohan11 <3292437251@qq.com>
2024-11-09 20:22:19 +0800

    11

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,18 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import pdf from "./components/pdf.vue";
 </script>
 
 <template>
-  <div>111
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <div style="width: 400px;">
-    <HelloWorld  />
+  <div class="cont">
+    <pdf />
   </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.cont {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
---
+++ src/components/pdf.vue
@@ -0,0 +1,46 @@
+<!-- 用于重构导出pdf的功能 -->
+
+<!-- html2canvas
+jspdf
+fileSave -->
+
+<template>
+  <div>
+    <el-button type="primary">Primary</el-button>
+
+    <div class="pdf_content">
+      <div class="pdf_item">
+        <div class="item_l">1</div>
+        <div class="item_r">2</div>
+      </div>
+      <div class="pdf_item"></div>
+      <div class="pdf_item"></div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.pdf_content {
+  width: 793px;
+  .pdf_item {
+    width: 100%;
+    height: 1122px;
+    margin-bottom: 20px;
+    background-color: #f5f5f5;
+    box-shadow: 2px 2px 8px #d6d6d6;
+    display: flex;
+    align-items: center;
+  }
+  .item_l {
+    width: 70%;
+    height: 100%;
+  }
+  .item_r {
+    width: 30%;
+    height: 100%;
+    border-left: 1px dashed #ccc;
+  }
+}
+</style>
